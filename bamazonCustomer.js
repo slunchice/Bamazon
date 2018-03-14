ef638c826ed8bc7148c093ddad81ac400be8a3fb
slunchice <slunchice_@hotmail.com>
2018-03-14 17:16:09 -0400

    changes to inquerer

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     readProducts();
-    start();
+    
 });
 
 function readProducts() {
@@ -25,67 +25,98 @@ function readProducts() {
 
         // Log all results of the SELECT statement
         console.log(res);
-        connection.end();
+        var inventory = res;
+        start(inventory);
+        // connection.end();
     });
 }
 
-function start() {
-    connection.query("SELECT * FROM products", function(err, results) {
-      if (err) throw err;
+function start(inventory) {
       inquirer
         .prompt([
           {
             name: "choice",
-            type: "rawlist",
-            choices: function() {
-              var choiceArray = [];
-              for (var i = 0; i < results.length; i++) {
-                choiceArray.push(results[i].item_name);
-              }
-              return choiceArray;
-            },
-            message: "What item would you like to buy? Type in the item_id"
-          },
-          {
-            name: "quantity",
             type: "input",
-            message: "How many items would you like to buy?"
-          }
-        ])
-        .then(function(answer) {
-          // get the information of the chosen item
-          var chosenItem;
-          for (var i = 0; i < results.length; i++) {
-            if (results[i].item_name === answer.choice) {
-              chosenItem = results[i];
+            message: "What item would you like to buy? Type in the item_id",
+            validate: function(val){
+              return !isNaN(val) || val.toLowerCase() === "q";
             }
+            // name: "available",
+            // type:"input"
           }
-  
-          // determine if bid was high enough
-          if (chosenItem.highest_bid < parseInt(answer.bid)) {
-            // bid was high enough, so update db, let the user know, and start over
-            connection.query(
-              "UPDATE products SET ? WHERE ?",
-              [
-                {
-                  highest_bid: answer.bid
-                },
-                {
-                  id: chosenItem.id
-                }
-              ],
-              function(error) {
-                if (error) throw err;
-                console.log("Bid placed successfully!");
-                start();
+          // {
+          //   name: "quantity",
+          //   type: "input",
+          //   message: "How many items would you like to buy?"
+          // }
+        ])
+        .then(function(val) {
+          // get the information of the chosen item
+          
+          // var chosenItem;
+          // for (var i = 0; i < results.length; i++) {
+          //   console.log(results);
+          //   if (results[i].item_id === answer.choice) {
+          //     chosenItem = results[i];
+          checkIfShouldExit(val.choice);
+            var choiceId = parseInt(val.choice);
+            var product = checkInventory(choiceId, inventory);
+            if (product) { 
+            promptUserForQuantity(product);
+              } else {
+                console.log("\nSorry that item is not in our inventory");
+                readProducts();
               }
-            );
+            })
+      }
+
+    function promptUserForQuantity(product){
+        inquirer
+          .prompt([
+            {
+              name: "quantity",
+              type: "input",
+              message: "How many items would you like to buy? [Quit with Q]",
+              validate: function(val){
+                return val > 0 || val.toLowerCase() === "q";
+              }
+            }
+          ])
+          .then(function(val){
+            var quantity = parseInt(val.quantity);
+            if (quantity > product.stock_quantity){
+            console.log("\nInsufficient quantity!")
+            readProducts();
           }
           else {
-            // bid wasn't high enough, so apologize and start over
-            console.log("Your bid was too low. Try again...");
-            start();
+            makePurchase(product, quantity);
           }
-        });
-    });
+        })
+      }
+
+    function makePurchase(product, quantity){
+      connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id=?",
+        [quantity, product.item_id],
+        function(err,res){
+        console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + " 's!");
+        readProducts();
+      }
+    )
+  };
+          
+  function checkInventory(choiceId, inventory) {
+    for (var i = 0; i < inventory.length; i++){
+      if (inventory[i].item_id === choiceId){
+        return inventory[i];
+      }
+    }
+    return null;
+  }
+
+  function checkIfShouldExit(choice){
+    if (choice.toLowerCase() === "q"){
+      console.log("Goodbye");
+      process.exit(0);
+    }
   }
