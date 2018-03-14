630c3fc7780dfb8f5117e44f0ef1817caf1d7145
slunchice <slunchice_@hotmail.com>
2018-03-14 18:26:11 -0400

    changes to README and fix to unsuccessful

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Bamazon
+
+Upon start, the app lists the inventory
+
+![amazon Capture ](\images\Bamazon Capture 1.PNG)
+
+then it prompts about type and quantity of the desired product!![img](/images/Bamazon%20Capture%202.PNG?lastModify=1521065561)
+
+
+
+
+
+![img](/images/Bamazon%20Capture%203.PNG?lastModify=1521065561)
+
+
+
+
+
+![amazon Capture ](\images\Bamazon Capture 3.PNG
+
+![amazon Capture ](\images\Bamazon Capture 4.PNG)
+
+![amazon Capture ](C:\Users\slunc\Desktop\Bamazon\Bamazon\images\Bamazon Capture 5.PNG)
+
+
+
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,10 +5,8 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
 
-    // Your username
     user: "root",
 
-    // Your password
     password: "woodpen10",
     database: "bamazon"
 });
@@ -20,14 +18,14 @@ connection.connect(function (err) {
 });
 
 function readProducts() {
-    connection.query("SELECT item_id, product_name, department_name, price FROM products", function (err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
-        // Log all results of the SELECT statement
+
         console.log(res);
         var inventory = res;
         start(inventory);
-        // connection.end();
+
     });
 }
 
@@ -37,30 +35,20 @@ function start(inventory) {
           {
             name: "choice",
             type: "input",
-            message: "What item would you like to buy? Type in the item_id",
+            message: "What item would you like to buy? Type in the item_id. [Quit with Q]",
             validate: function(val){
               return !isNaN(val) || val.toLowerCase() === "q";
             }
-            // name: "available",
-            // type:"input"
+       
           }
-          // {
-          //   name: "quantity",
-          //   type: "input",
-          //   message: "How many items would you like to buy?"
-          // }
+          
         ])
         .then(function(val) {
-          // get the information of the chosen item
-          
-          // var chosenItem;
-          // for (var i = 0; i < results.length; i++) {
-          //   console.log(results);
-          //   if (results[i].item_id === answer.choice) {
-          //     chosenItem = results[i];
+    
           checkIfShouldExit(val.choice);
             var choiceId = parseInt(val.choice);
             var product = checkInventory(choiceId, inventory);
+            console.log(product);
             if (product) { 
             promptUserForQuantity(product);
               } else {
@@ -84,8 +72,10 @@ function start(inventory) {
           ])
           .then(function(val){
             var quantity = parseInt(val.quantity);
+            console.log(quantity);
+            console.log(product.stock_quantity);
             if (quantity > product.stock_quantity){
-            console.log("\nInsufficient quantity!")
+            console.log("\nInsufficient quantity!");
             readProducts();
           }
           else {
@@ -96,7 +86,7 @@ function start(inventory) {
 
     function makePurchase(product, quantity){
       connection.query(
-        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id=?",
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
         [quantity, product.item_id],
         function(err,res){
         console.log("\nSuccessfully purchased " + quantity + " " + product.product_name + " 's!");
